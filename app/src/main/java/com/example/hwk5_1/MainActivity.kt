fbf4fa73cda29fbd89a7f857c8d9a0e24abb5d28
nekoo <t106360107@ntut.edu.tw>
2019-12-09 17:19:24 +0800

    HWK5

---
+++ app/src/main/java/com/example/hwk5_1/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.hwk5_1
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            val intent = Intent(this@MainActivity, MyService::class.java)
+            startService(intent)
+            Toast.makeText(this@MainActivity, "啟動Service", Toast.LENGTH_LONG).show()
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hwk5_1/MyService.kt
@@ -0,0 +1,33 @@
+package com.example.hwk5_1
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MyService : Service() {
+    override fun onCreate() {
+        super.onCreate()
+        Thread(Runnable {
+            try {
+                Thread.sleep(2000)
+                val intent = Intent(this@MyService, Main2Activity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                this@MyService.startActivity(intent)
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }).start()
+
+        stopSelf()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+}
